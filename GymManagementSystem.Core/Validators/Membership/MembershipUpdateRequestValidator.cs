438f967b95b5f29d985a656fe1ac01425238c3cd
Emrimde <72688765+Emrimde@users.noreply.github.com>
2026-01-24 17:58:54 +0100

    feat: create fluent validations for MembershipUpdateRequest

---
+++ GymManagementSystem.Core/Validators/Membership/MembershipUpdateRequestValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using GymManagementSystem.Core.DTO.Membership;
+
+namespace GymManagementSystem.Core.Validators.Membership;
+public class MembershipUpdateRequestValidator : AbstractValidator<MembershipUpdateRequest>
+{
+    public MembershipUpdateRequestValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty().WithMessage("Name is required.")
+            .MinimumLength(10).WithMessage("Name must have at least 10 characters.")
+            .MaximumLength(50).WithMessage("Name cannot exceed 50 characters.");
+
+        RuleFor(x => x.ClassBookingDaysInAdvanceCount)
+            .GreaterThanOrEqualTo(0).WithMessage("Value cannot be negative.");
+
+        RuleFor(x => x.FreeFriendEntryCountPerMonth)
+            .GreaterThanOrEqualTo(0).WithMessage("Value cannot be negative.");
+
+        RuleFor(x => x.FreePersonalTrainingSessions)
+            .GreaterThanOrEqualTo(0).WithMessage("Value cannot be negative.");
+    }
+}
