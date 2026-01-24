b57f440090e4b7f3bc29dc84aa2b4e5de34c6755
Emrimde <72688765+Emrimde@users.noreply.github.com>
2026-01-24 09:38:28 +0100

    feat: create fluent validations for membership feature request dtos

---
+++ GymManagementSystem.Core/Validators/MembershipFeature/MembershipFeatureAddRequestValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+using GymManagementSystem.Core.DTO.MembershipFeature;
+
+namespace GymManagementSystem.Core.Validators.MembershipFeature;
+public class MembershipFeatureAddRequestValidator : AbstractValidator<MembershipFeatureAddRequest>
+{
+    public MembershipFeatureAddRequestValidator()
+    {
+        RuleFor(item => item.MembershipId).NotEmpty().WithMessage("MembershipId is empty");
+        RuleFor(item => item.FeatureDescription).NotEmpty().WithMessage("Feature description is empty");
+    }
+}
---
+++ GymManagementSystem.Core/Validators/MembershipFeature/MembershipFeatureUpdateRequestValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+using GymManagementSystem.Core.DTO.MembershipFeature;
+
+namespace GymManagementSystem.Core.Validators.MembershipFeature;
+public class MembershipFeatureUpdateRequestValidator : AbstractValidator<MembershipFeatureUpdateRequest>
+{
+    public MembershipFeatureUpdateRequestValidator()
+    {
+        RuleFor(item => item.MembershipFeatureId).NotEmpty().WithMessage("MembershipId is empty");
+        RuleFor(item => item.FeatureDescription).NotEmpty().WithMessage("Feature description is empty");
+    }
+}
