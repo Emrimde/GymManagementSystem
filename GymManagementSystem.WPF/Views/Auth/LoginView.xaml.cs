7a364e4608a57c1b19b01311819418fefe95fba1
Emrimde <72688765+Emrimde@users.noreply.github.com>
2026-01-29 19:24:05 +0100

    feat: init material design

--- GymManagementSystem.WPF/Views/Auth/LoginView.xaml.cs
+++ GymManagementSystem.WPF/Views/Auth/LoginView.xaml.cs
@@ -1,10 +1,19 @@
-﻿using System.Windows.Controls;
+﻿using GymManagementSystem.WPF.ViewModels.Auth;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace GymManagementSystem.WPF.Views.Auth;
 public partial class LoginView : UserControl
 {
     public LoginView()
     {
+
         InitializeComponent();
+    }
+
+    private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+    {
+        if (DataContext is LoginViewModel vm)
+            vm.Password = ((PasswordBox)sender).Password;
     }
 }
